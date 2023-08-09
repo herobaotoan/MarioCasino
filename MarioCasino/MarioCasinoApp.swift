e1766fc00f6d1ead6603db8fe63a5f90faf6ab12
herobaotoan <chitoan.tran@yahoo.com.vn>
2023-08-09 12:35:21 +0700

    Initial Commit

---
+++ MarioCasino/MarioCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioCasinoApp.swift
+//  MarioCasino
+//
+//  Created by Toan Tran Chi on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarioCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
