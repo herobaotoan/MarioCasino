3563e96a90bac72b8d9af816d08ca9c39accaef8
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2023-08-23 14:20:38 +0700

    Update EnemyView.swift

--- MarioCasino/EnemyView.swift
+++ MarioCasino/EnemyView.swift
@@ -1,5 +1,5 @@
 //
-//  DiceView.swift
+//  Enemy.swift
 //  MarioCasino
 //
 //  Created by Toan Tran Chi on 09/08/2023.
@@ -10,28 +10,34 @@ import SwiftUI
 struct EnemyView: View {
     var level: Int
     var HP: Int
-
+    
+    @Binding var hit: Bool
+    
     let names = ["Goomba", "Koopa", "Piranha Plant", "Boo", "Magikoopa", "Bowser"]
     var body: some View {
         VStack{
             HStack{
+                Spacer()
                 Text(names[level])
                     .modifier(TextWhiteModifier(fontSize: 20))
                     .modifier(CapsuleColorModifier(color: Color.black.opacity(0.3)))
-                Spacer()
+                
                 Text("HP: \(HP)")
                     .modifier(TextWhiteModifier(fontSize: 20))
                     .modifier(CapsuleColorModifier(color: Color.black.opacity(0.3)))
+                Spacer()
             }
             Image(names[level])
                 .resizable()
                 .frame(width: 300, height: 300)
+                .offset(x: hit ? -10 : 0)
+                .animation(Animation.default.repeatCount(5))
         }
     }
 }
 
-struct EnemyView_Previews: PreviewProvider {
-    static var previews: some View {
-        EnemyView(level: 0, HP: 30)
-    }
-}
+//struct EnemyView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EnemyView(level: 0, HP: 30,)
+//    }
+//}
