8106a6a4b29bbc482633483b7dc51289a46ee291
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2023-08-30 14:26:36 +0700

    Update MenuView.swift

--- MarioCasino/MenuView.swift
+++ MarioCasino/MenuView.swift
@@ -8,36 +8,36 @@
 import SwiftUI
 
 struct MenuView: View {
+    @Binding var enable: Bool
+    @Binding var howToPlayViewEnable: Bool
+    @Binding var leaderboardViewEnable: Bool
     var body: some View {
-        @Binding var enable: Bool
-        @Binding var howToPlayViewEnable: Bool
-        @Binding var leaderboardViewEnable: Bool
         ZStack {
             //Background
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.red]), startPoint: .leading, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 //LOGO
-                Image("logo")
+                Image("banner")
                     .resizable()
                     .scaledToFit()
                     .frame(minHeight: 130, idealHeight: 170, maxHeight: 200, alignment: .center)
                 Button{
-                    informationUI = true
+                    enable = false
                 } label: {
                     Text("PLAY")
                         .modifier(TextWhiteModifier(fontSize: 20))
                         .modifier(CapsuleColorModifier(color: Color.black.opacity(0.7)))
                 }
                 Button{
-                    informationUI = true
+                    howToPlayViewEnable = true
                 } label: {
                     Text("HOW TO PLAY")
                         .modifier(TextWhiteModifier(fontSize: 20))
                         .modifier(CapsuleColorModifier(color: Color.black.opacity(0.7)))
                 }
                 Button{
-                    informationUI = true
+                    leaderboardViewEnable = true
                 } label: {
                     Text("LEADERBOARD")
                         .modifier(TextWhiteModifier(fontSize: 20))
